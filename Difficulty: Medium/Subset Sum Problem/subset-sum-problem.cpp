c155ade14e2326fca5a24210d8522374ba51fdcd
Ashutosh Jaiswal <90177888+Ashutoshxii@users.noreply.github.com>
2024-11-07 19:37:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,67 +1,70 @@
 //{ Driver Code Starts
-//Initial template for C++
 
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
+
 
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
-class Solution{   
-public:
-    bool isSubsetSum(vector<int>arr, int sum){
-        // code here 
-        int n = arr.size() ;
-        // bool arra[n+1][sum+1] ;
-        // for(int i = 0 ; i < n+1 ; i++){
-        //     for(int j = 0 ; j < sum+1 ; j++){
-        //         if(i==0){
-        //             arra[i][j] = false ;
-        //         }
-        //         if(j==0){
-        //             arra[i][j] = true;
-        //         }
-        //     }
-        // }
-        vector<vector<bool>> arra(n + 1, vector<bool>(sum + 1, false));
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int row = arr.size() + 1 ;
+        int col = sum + 1 ;
+        // vector<vector<bool>> vec (row,vector<bool>(col)) ;
+        // return 1 ;
+         vector<vector<bool>> vec(row, vector<bool>(col, false));
 
-        for (int i = 0; i <= n; i++) {
-            arra[i][0] = true; 
+        for (int i = 0; i < row; i++) {
+            vec[i][0] = true; 
         }
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j < sum+1 ; j++){
+        
+        
+        
+        for(int i = 1 ; i < row ; i++){
+            for(int j = 1 ; j < col ; j++){
                 if(arr[i-1]<=j){
-                    arra[i][j] = arra[i-1][j-arr[i-1]]||arra[i-1][j] ;
+                    vec[i][j] = (vec[i-1][j] || vec[i-1][j-arr[i-1]]);
                 }
                 else{
-                    arra[i][j] = arra[i-1][j] ; 
+                    vec[i][j] = vec[i-1][j] ; 
                 }
             }
         }
-        return arra[n][sum] ;
-        
+        return vec[row-1][col-1] ;
     }
 };
 
 //{ Driver Code Starts.
-int main() 
-{ 
+
+int main() {
+
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int N, sum;
-        cin >> N;
-        vector<int> arr(N);
-        for(int i = 0; i < N; i++){
-            cin >> arr[i];
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
+        int sum;
         cin >> sum;
-        
+        cin.ignore();
+
         Solution ob;
-        cout << ob.isSubsetSum(arr, sum) << endl;
+        if (ob.isSubsetSum(arr, sum))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
     }
-    return 0; 
-} 
+    return 0;
+}
 
 // } Driver Code Ends
