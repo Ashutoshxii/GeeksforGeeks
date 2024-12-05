07a7d543d6a3d78e03b6355663423a2d8c1661d6
Ashutosh Jaiswal <90177888+Ashutoshxii@users.noreply.github.com>
2024-12-05 19:51:21 +0530

    Update longest-increasing-subsequence.cpp

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -9,53 +9,97 @@ using namespace std;
 class Solution {
   public:
     // Function to find length of longest increasing subsequence.
-    int solvetab(vector<int> arr){
+    int solve(vector<int>&arr , int curr , int prev){
         int n = arr.size() ; 
-        vector<vector<int>> dp ( n + 1 , vector<int> ( n+1 , 0)) ; 
-        
-        for(int curr = n - 1 ; curr>= 0 ; curr--){
-            for(int prev = curr-1 ; prev>= -1 ; prev--){
-                int take = 0 ; 
-                if(prev==-1|| arr[curr]>arr[prev]){
-                    take = 1 + dp[curr+1][curr+1] ;
-                }
-                int untake = dp[curr+1][prev+1] ; 
-                dp[curr][prev+1] = max(take, untake) ; 
-            }
+        if(curr==n)return 0 ;
+        int take = 0 ; 
+        if(prev==-1||arr[curr]>arr[prev]){
+            take = 1 + solve(arr , curr+ 1 , curr) ;
         }
-        return dp[0][0];
+        int untake = solve(arr, curr+1,prev);
+        return max(take,untake) ; 
+        //4min
     }
-    int solvefast(vector<int> arr){
+    int solvemem(vector<int>&arr , int curr , int prev, vector<vector<int>> dp){
         int n = arr.size() ; 
-        // vector<vector<int>> dp ( n + 1 , vector<int> ( n+1 , 0)) ; 
-        vector<int>currRow (n+1,0 ) ; 
-        vector<int>nextRow (n+1,0 ) ; 
+        if(curr==n)return 0 ;
         
-        for(int curr = n - 1 ; curr>= 0 ; curr--){
-            for(int prev = curr-1 ; prev>= -1 ; prev--){
-                int take = 0 ; 
-                if(prev==-1|| arr[curr]>arr[prev]){
-                    // take = 1 + dp[curr+1][curr+1] ;
-                    take = 1 + nextRow[curr+1] ; 
-                    // take = 1 + curr[curr+1] ; 
-                }
-                // int untake = dp[curr+1][prev+1] ; 
-                int untake = nextRow[prev+1] ; 
-                // int untake = curr 
-                
-                currRow[prev+1] = max(take, untake) ; 
-            }
-            // currRow = nextRow ; 
-            nextRow = currRow ; 
+        if(dp[curr][prev+1]!=-1){
+            return dp[curr][prev+1] ; 
         }
-        // return dp[0][0];
+        
+        int take = 0 ; 
+        if(prev==-1||arr[curr]>arr[prev]){
+            take = 1 + solvemem(arr , curr+ 1 , curr , dp) ;
+        }
+        int untake = solvemem(arr, curr+1,prev,dp);
+        return dp[curr][prev+1]  = max(take,untake) ; 
+        //6min
+    }
+    int tabulation(vector<int>arr,int n){
+        vector<vector<int>> dp (n+1 , vector<int> (n+1, 0));
+        
+        for(int curr = n -1 ; curr>= 0 ; curr--){
+            for(int prev = curr - 1 ; prev >= -1 ; prev--){
+                int take = 0 ; 
+                if(prev==-1||arr[curr]>arr[prev]){
+                    take = 1 + dp[curr+ 1][ curr + 1 ] ;
+                }
+                int untake = dp[curr+1][prev+1];
+                // return dp[curr][prev+1]  = max(take,untake) ; 
+                dp[curr][prev+1] = max(take , untake) ; 
+            }
+        }
+        return dp[0][0] ; 
+        
+    }
+    int tabulationfast(vector<int>arr,int n){
+        // vector<vector<int>> dp (n+1 , vector<int> (n+1, 0));
+        vector<int> currRow( n + 1 , 0) ; 
+        vector<int> nextRow( n + 1 , 0) ; 
+        
+        for(int curr = n -1 ; curr>= 0 ; curr--){
+            for(int prev = curr - 1 ; prev >= -1 ; prev--){
+                int take = 0 ; 
+                if(prev==-1||arr[curr]>arr[prev]){
+                    take = 1 + nextRow[ curr + 1 ] ;
+                }
+                int untake = nextRow[prev+1];
+                // return dp[curr][prev+1]  = max(take,untake) ; 
+                currRow[prev+1] = max(take , untake) ; 
+            }
+            nextRow  = currRow ; 
+        }
         return currRow[0] ; 
         
+    }
+    // int 
+    int solveoptimal(vector<int> arr , int n ){
+        if(n==0){
+            return 0 ; 
+        }
+        vector<int> vec ; 
+        vec.push_back(arr[0]) ; 
+        for(int i = 1 ; i  < n ; i++){
+            if(vec.back()<arr[i]){
+                vec.push_back(arr[i]) ; 
+            }
+            else{
+                int j = lower_bound(vec.begin() , vec.end() , arr[i]) - vec.begin()  ;
+                vec[j] = arr[i] ; 
+            }
+        }
+        return vec.size() ; 
         
     }
     int longestSubsequence(vector<int>& arr) {
         // code here
-        return solvefast(arr); 
+        // int n = arr.size() ; 
+        // vector<vector<int>> dp (n , vector<int>(n+1 , -1));
+        // return solvemem(arr , 0 , -1 , dp) ; 
+        int n = arr.size() ; 
+        // return tabulationfast(arr,n) ;
+        return solveoptimal(arr,n) ; 
     }
 };
 
