3448f2f75e3c44f58c6f093d577a97dda98319c8
Ashutosh Jaiswal <90177888+Ashutoshxii@users.noreply.github.com>
2025-10-07 17:31:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,64 +1,30 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to return a list containing the DFS traversal of the graph.
-    void solve(int node ,vector<bool>& visited ,vector<int>&ans,vector<vector<int>>& adj){
-        visited[node] = true ; 
-        ans.push_back(node) ; 
-        
-        // for(int i = 0 ; i< adj[node].size() ;i++){
-        //     if(!visited[adj[node][i] ]){
-        //         solve( adj[node][i] , visited , ans , adj ) ;
-        //     }
-        // }
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                solve(neighbor, visited, ans, adj); // Recursively visit unvisited neighbors
+  
+    
+    void solve(vector<vector<int>>&adj , vector<int>&ans,int node,vector<bool>&visited){
+        if(!visited[node]){
+            visited[node] = 1 ; 
+            ans.push_back(node) ; 
+            for(int i : adj[node]){
+                solve(adj,ans,i,visited) ; 
             }
         }
+        return ; 
+        
     }
-    vector<int> dfsOfGraph(vector<vector<int>>& adj) {
+    vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
         vector<int> ans ; 
-        vector<bool> visited(adj.size() , 0) ; 
-        solve(0 , visited ,ans  , adj) ; 
+        vector<bool>visited(adj.size()) ; 
+        
+        // for(int i : adj[0]){
+        solve(adj,ans,0,visited) ; 
+        // }
+        
         return ans ; 
+        
+        
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        vector<vector<int>> adj(
-            V); // Use vector of vectors instead of array of vectors.
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        Solution obj;
-        vector<int> ans = obj.dfsOfGraph(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
